cc73588d4ebc300ac324477ccdb9f173e166582b
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-03-01 13:02:20 -0700

    add check for formating emm_population_weights and emm_to_states

--- format_emm_population_weights.py
+++ format_emm_population_weights.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import numpy as np
 
 emm_populaiton_weights = (
         pd.read_csv('convert_data/geo_map/EMM_National.txt', sep = '\t')
         )
+
+# check
+assert np.isclose(
+        emm_populaiton_weights["Population Weight"].to_numpy(),
+        np.array(emm_populaiton_weights["Total Population"] / emm_populaiton_weights["Total Population"].sum()),
+        rtol = 1e-3).all()
 
 emm_populaiton_weights.to_parquet("parquets/emm_populaiton_weights.parquet")
--- format_emm_to_states.py
+++ format_emm_to_states.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 emm_to_states = (
         pd.read_csv('convert_data/geo_map/EMM_State_RowSums.txt', sep = '\t')
@@ -7,5 +8,15 @@ emm_to_states = (
               var_name = "State",
               value_name = 'emm_to_state_factor')
         )
+
+# Verify that the sum of the emm_to_state_factor colum is 1 for each EMM
+assert (
+        emm_to_states
+        .groupby(["EMM"])
+        .agg({"emm_to_state_factor":"sum"})
+        .emm_to_state_factor
+        .apply(lambda x : math.isclose(a = x, b = 1.0, rel_tol = 1e-8))
+        .all()
+         )
 
 emm_to_states.to_parquet("parquets/emm_to_states.parquet")
